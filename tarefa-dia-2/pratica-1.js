d9a0c2263164e98273916dccf6d849782b404ae4
Luiza Ocker <luiza.or@hotmail.com>
2022-08-24 12:08:46 -0300

    Tarefas

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,11 @@
-let nome
-let email
-let profissao
-let idade
-let lugar
+let nome = "Luiza Ocker Ribeiro"
+let email = "luiza.or@hotmail.com"
+let profissao = "Ilustrator"
+let idade = 26 
+let lugar = "Japan"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
