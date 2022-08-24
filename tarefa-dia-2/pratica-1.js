82c5e0a1c9a5642f828eb827e44a9803e6bceaf7
Luiza Ocker <luiza.or@hotmail.com>
2022-08-24 12:27:20 -0300

    Tarefas

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,13 @@
 let nome = "Luiza Ocker Ribeiro"
 let email = "luiza.or@hotmail.com"
-let profissao = "Ilustrator"
+let profissao = "Ilustradora"
 let idade = 26 
-let lugar = "Japan"
+let lugar = "Japão"
 
-console.log(nome)
-console.log(email)
-console.log(profissao)
-console.log(idade)
-console.log(lugar)
+//console.log(nome)
+//console.log(email)
+//console.log(profissao)
+//console.log(idade)
+//console.log(lugar)
+
+console.log("Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
